f8f72b264b58a9833aba2a7bf04d379aac80b2cc
Kyle Rich <kyledvrich@gmail.com>
2022-09-27 16:29:48 -0700

    changed script not to run antlr every time (unnecessary)

--- public/client-bundle.js
+++ public/client-bundle.js
@@ -8,7 +8,6 @@ var __webpack_unused_export__;
 
 __webpack_unused_export__ = ({ value: true });
 function handleMapOutputStatements() {
-    console.log('inside handle');
     const xhr = new XMLHttpRequest();
     const url = '/';
     xhr.open('GET', url, false);
@@ -16,16 +15,13 @@ function handleMapOutputStatements() {
     xhr.onload = () => {
         if (xhr.status === 200) {
             const listOfOutputStatements = JSON.parse(xhr.response).result;
-            console.log(listOfOutputStatements);
             return renderMap(listOfOutputStatements);
         }
         else {
             const err = JSON.parse(xhr.response).error;
-            console.log(err);
             return renderMap(err);
         }
     };
-    return null;
 }
 function renderMap(outputStatements) {
     for (const statement of outputStatements) {
