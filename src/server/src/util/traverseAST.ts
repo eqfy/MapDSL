8fa1bc8b5405278906c46ea1f31de40c687b6c37
Kyle Rich <kyledvrich@gmail.com>
2022-09-28 17:21:56 -0700

    changes to traverseAST

--- src/server/src/util/traverseAST.ts
+++ src/server/src/util/traverseAST.ts
@@ -1,6 +1,8 @@
-import { ASTNode, ASTTokenNode, isASTNode, isASTTokenNode } from '../parser/AST';
+import { ASTNode, isASTNode, isASTTokenNode } from '../parser/AST';
 
-export function traverseAST(astNode: ASTNode | ASTTokenNode, actionOnNode: (node: ASTNode | ASTTokenNode) => void): void {
+export function traverseAST(astNode: ASTNode, actionOnNode: (node: ASTNode) => void): void {
+  if (!isASTNode(astNode)) return;
+
   actionOnNode(astNode);
   for (const propertyValue of Object.values(astNode)) {
     if (!(Array.isArray(propertyValue) || isASTTokenNode(propertyValue) || isASTNode(propertyValue))) continue;
