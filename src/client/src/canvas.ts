b9fce78545147101f37a2bb5120bd4ab15938efd
aidanperras <aidan234@students.cs.ubc.ca>
2022-10-07 01:01:52 -0700

    Added object mapping to canvas from list. TODO: fix pathway issue (I think I'm having issues with my build. Have map center at 0,0 instead of centered at canvas frame. Try and smooth out issues with lat and long being off by a bit (doing some research and played around for a few hours but couldn't find a better solution.

--- src/client/src/canvas.ts
+++ src/client/src/canvas.ts
@@ -1,3 +1,4 @@
+import { CoordinateUtils } from "./coordinateUtils";
 import { CreateStatement, isMarkerOutput, isStreetOutput, MarkerOutput, StreetOutput } from "./CreateStatement";
 
 // The class with everything related to rendering
@@ -35,11 +36,11 @@ export class Canvas {
             // then create corresponding google components by layers (bottom to top: polylines, markers)
             // for instead of forEach used here to avoid this keyword context issue within closure
             for (let i = 0; i < streetOutputs.length; i ++) {
-                polylines.push(this.createPolyline(streetOutputs[i]));
+                polylines.push(this.createPolyline(streetOutputs[i], map));
             }
 
             for (let i = 0; i < markerOutputs.length; i ++) {
-                markers.push(this.createMarker(markerOutputs[i]));
+                markers.push(this.createMarker(markerOutputs[i], map));
             }
         }
         this.polylines = polylines;
@@ -50,15 +51,52 @@ export class Canvas {
 
     // generate a google polyline based on street configuration
     // TODO: implementation
-    createPolyline(streetOutput: StreetOutput): google.maps.Polyline {
+    createPolyline(streetOutput: StreetOutput, map: google.maps.Map): google.maps.Polyline {
         console.log(`Created a polyline for streetOutput: ${JSON.stringify(streetOutput)}`);
-        return new google.maps.Polyline();
+        var color;
+        if (streetOutput.type == "Highway") {
+            color = "#ff9900";
+        } else if (streetOutput.type == "Bridge") {
+            color = "#b87333";
+        } else if (streetOutput.type == "Street") {
+            color = "#FFFFFF";
+        } else {
+            console.log(`ERROR GIVEN INVALID TYPE`);
+            color = "#FF0000";
+        }
+        const path = new google.maps.Polyline({
+            path: [CoordinateUtils.convertCoordinateToLatLng(streetOutput.startPosition),
+                   CoordinateUtils.convertCoordinateToLatLng(streetOutput.endPosition)],
+            strokeColor: color,
+            strokeOpacity: 1.0,
+            strokeWeight: 5,
+            map: map
+          });
+        return path;
     }
 
     // generate a google marker based on marker configuration
     // TODO: implementation
-    createMarker(markerOutput: MarkerOutput): google.maps.Marker {
+    createMarker(markerOutput: MarkerOutput, map: google.maps.Map): google.maps.Marker {
         console.log(`Created a marker for streetOutput: ${JSON.stringify(markerOutput)}`);
-        return new google.maps.Marker();
+        var imagePath;
+        if (markerOutput.type == "BusStop") {
+            
+        } else if (markerOutput.type == "StopSign") {
+            
+        } else if (markerOutput.type == "TrafficLight") {
+            
+        } else if (markerOutput.type == "TrainStop"){
+        
+        } else {
+            console.log(`ERROR GIVEN INVALID TYPE`);
+            
+        }
+        const marker: google.maps.Marker = new google.maps.Marker({
+            position:  CoordinateUtils.convertCoordinateToLatLng(markerOutput.position),
+            map,
+            icon: "./client/assets/bus_stop.svg"
+          });
+        return marker;
     }
 }
