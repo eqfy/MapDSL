bf646bc96e532bd3ea80f1dc7672933549719074
Kyle Rich <kyledvrich@gmail.com>
2022-10-07 20:06:32 -0700

    checks

--- src/server/src/ast/Position.ts
+++ src/server/src/ast/Position.ts
@@ -10,6 +10,12 @@ export default class Position extends ASTNode {
 
   constructor(xCoordinate?: Expression, yCoordinate?: Expression, variableName?: TokenNode) {
     super();
+    if (variableName && (xCoordinate || yCoordinate)) {
+      throw new Error('if a variable name is specified, then the (expression,expression) should not be');
+    }
+    if (!variableName && ((yCoordinate && !xCoordinate) || (!yCoordinate && xCoordinate))) {
+      throw new Error('if a (x,y) is specified, then both x and y must be specified');
+    }
     this.xCoordinate = xCoordinate;
     this.yCoordinate = yCoordinate;
     this.variableName = variableName;
