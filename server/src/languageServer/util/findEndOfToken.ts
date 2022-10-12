8f315b524417a77391b03d2ec97ef34bfbb1d160
Kyle Rich <kyledvrich@gmail.com>
2022-10-11 19:33:11 -0700

    fixed logic in finding end of token

--- server/src/languageServer/util/findEndOfToken.ts
+++ server/src/languageServer/util/findEndOfToken.ts
@@ -4,7 +4,7 @@ import { Range , Position} from 'vscode-languageserver/node';
 export function findEndOfToken(textDocument: TextDocument, line: number, column: number): Position{
   const currRange: Range = {start: { line: line, character: column }, end: { line: line, character: column + 1 }};
   let currChar =  textDocument.getText(currRange);
-  while(currChar.match(/\s/)) {
+  while(!currChar.match(/\s/)) {
     currRange.start.character += 1;
     currRange.end.character += 1;
     currChar = textDocument.getText(currRange);
